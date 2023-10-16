d552ff07d993b28e92e34e4105f20f2e4d5f7e44
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-16 15:31:05 +0900

    .

--- oraclelinux/server-setup.sh
+++ oraclelinux/server-setup.sh
@@ -21,24 +21,27 @@ echo '(1/${TOTAL_STEP}) dnf 업데이트 완료!'
 
 echo '(2/${TOTAL_STEP}) 도커 설치 시작'
 
-if [ -z "$DOCKER_USER" ]; then
-  echo "사용자명을 제공해야 합니다."
-  exit 1
-fi
-
-# 도커 설치
-sudo dnf install -y docker
-# 도커 서비스 시작 및 활성화
-sudo systemctl start docker
-sudo systemctl enable docker
-# Docker 그룹 생성 (이미 있는 경우에는 생략 가능)
-sudo groupadd docker
-# 사용자를 Docker 그룹에 추가
-sudo usermod -aG docker $DOCKER_USER
-# 사용자 변경 내용 적용
-su - $DOCKER_USER
-echo "Docker가 설치되었고, $DOCKER_USER 사용자가 Docker 그룹에 추가되었습니다."
-
+if ! command -v docker &> /dev/null; then
+    echo "Docker가 이미 설치되어 있습니다. 도커 설치를 건너뜁니다."
+else
+	if [ -z "$DOCKER_USER" ]; then
+	  echo "사용자명을 제공해야 합니다."
+	  exit 1
+	fi
+	
+	# 도커 설치
+	sudo dnf install -y docker
+	# 도커 서비스 시작 및 활성화
+	sudo systemctl start docker
+	sudo systemctl enable docker
+	# Docker 그룹 생성 (이미 있는 경우에는 생략 가능)
+	sudo groupadd docker
+	# 사용자를 Docker 그룹에 추가
+	sudo usermod -aG docker $DOCKER_USER
+	# 사용자 변경 내용 적용
+	su - $DOCKER_USER
+	echo "Docker가 설치되었고, $DOCKER_USER 사용자가 Docker 그룹에 추가되었습니다."
+	
 echo '(2/${TOTAL_STEP}) 도커 설치 완료!'
 
 
