a241521fe835c28c0f2d14d0315e4a86f7bdc6cc
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-16 15:15:22 +0900

    .

--- oraclelinux/server-setup.sh
+++ oraclelinux/server-setup.sh
@@ -9,11 +9,31 @@ read DOCKER_USER
 echo '(1/${TOTAL_STEP}) dnf 업데이트'
 dnf update -y
 
+
+
+
+
 echo '(2/${TOTAL_STEP}) 도커 설치'
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce
+
+if [ -z "$DOCKER_USER" ]; then
+  echo "사용자명을 제공해야 합니다."
+  exit 1
+fi
+
+# 도커 설치
+sudo dnf install -y docker
+# 도커 서비스 시작 및 활성화
 sudo systemctl start docker
 sudo systemctl enable docker
+# Docker 그룹 생성 (이미 있는 경우에는 생략 가능)
+sudo groupadd docker
+# 사용자를 Docker 그룹에 추가
 sudo usermod -aG docker $DOCKER_USER
+# 사용자 변경 내용 적용
+su - $DOCKER_USER
+echo "Docker가 설치되었고, $DOCKER_USER 사용자가 Docker 그룹에 추가되었습니다."
+
+
+
 
 echo '(3/${TOTAL_STEP}) gitalb 설치'
