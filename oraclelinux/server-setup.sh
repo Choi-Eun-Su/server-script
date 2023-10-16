dd54e02e2d524ca0fb8752c1c85f694159b11d63
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-16 15:22:55 +0900

    .

--- oraclelinux/server-setup.sh
+++ oraclelinux/server-setup.sh
@@ -6,14 +6,15 @@ echo '유틸 서버 셋업 시작...'
 echo '도커 사용자 이름을 입력 : '
 read DOCKER_USER
 
-echo '(1/${TOTAL_STEP}) dnf 업데이트'
+echo '(1/${TOTAL_STEP}) dnf 업데이트 시작'
 dnf update -y
+echo '(1/${TOTAL_STEP}) dnf 업데이트 완료!'
 
 
 
 
 
-echo '(2/${TOTAL_STEP}) 도커 설치'
+echo '(2/${TOTAL_STEP}) 도커 설치 시작'
 
 if [ -z "$DOCKER_USER" ]; then
   echo "사용자명을 제공해야 합니다."
@@ -32,6 +33,8 @@ sudo usermod -aG docker $DOCKER_USER
 # 사용자 변경 내용 적용
 su - $DOCKER_USER
 echo "Docker가 설치되었고, $DOCKER_USER 사용자가 Docker 그룹에 추가되었습니다."
+
+echo '(2/${TOTAL_STEP}) 도커 설치 완료!'
 
 
 
