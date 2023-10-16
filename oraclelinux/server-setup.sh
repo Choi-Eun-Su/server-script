6bbac8b826661cbd4c5f1124c63f1cdee2410bbe
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-16 15:26:27 +0900

    .

--- oraclelinux/server-setup.sh
+++ oraclelinux/server-setup.sh
@@ -3,8 +3,13 @@ SLEEP_TIME=10
 
 echo '유틸 서버 셋업 시작...'
 
-echo '도커 사용자 이름을 입력 : '
+echo '2.도커 사용자 이름을 입력 : '
 read DOCKER_USER
+
+echo '3-1. gitlab 볼륨 폴더 경로 : '
+read GITLAB_VOLUME_DIR
+echo '3-2. GITLAB HOSTNAME : '
+read GITLAB_HOSTNAME
 
 echo '(1/${TOTAL_STEP}) dnf 업데이트 시작'
 dnf update -y
@@ -39,4 +44,48 @@ echo '(2/${TOTAL_STEP}) 도커 설치 완료!'
 
 
 
-echo '(3/${TOTAL_STEP}) gitalb 설치'
+echo '(3/${TOTAL_STEP}) gitalb 설치 시작'
+# GitLab 컨테이너를 실행하기 위한 변수 설정
+GITLAB_CONTAINER_NAME=gitlab
+GITLAB_PORT=80
+GITLAB_HTTPS_PORT=443
+GITLAB_SSH_PORT=22
+# GITLAB_VOLUME_DIR=/storage/gitlab
+# GITLAB_HOSTNAME=gitlab.example.com
+GITLAB_IMAGE=gitlab/gitlab-ce:latest
+
+# Docker를 설치
+echo "Docker 설치 중..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+rm get-docker.sh
+
+# Docker 서비스 시작 및 활성화
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# GitLab 컨테이너 실행
+echo "GitLab 컨테이너 실행 중..."
+docker run --detach \
+  --hostname $GITLAB_HOSTNAME \
+  --publish $GITLAB_HTTPS_PORT:443 --publish $GITLAB_PORT:80 --publish $GITLAB_SSH_PORT:22 \
+  --name $GITLAB_CONTAINER_NAME \
+  --restart always \
+  --volume $GITLAB_VOLUME_DIR/config:/etc/gitlab \
+  --volume $GITLAB_VOLUME_DIR/logs:/var/log/gitlab \
+  --volume $GITLAB_VOLUME_DIR/data:/var/opt/gitlab \
+  $GITLAB_IMAGE
+
+# GitLab 초기 설정을 위한 대기
+echo "GitLab 컨테이너가 실행 중입니다. 초기 설정을 위해 잠시 기다려주세요..."
+sleep 60
+
+# GitLab 초기 설정
+docker exec -it $GITLAB_CONTAINER_NAME gitlab-ctl reconfigure
+
+# GitLab 컨테이너 다시 시작
+docker restart $GITLAB_CONTAINER_NAME
+
+echo "GitLab이 설정되었습니다. 웹 브라우저에서 http://$GITLAB_HOSTNAME 에서 액세스하세요."
+echo '(3/${TOTAL_STEP}) gitalb 설치 완료'
