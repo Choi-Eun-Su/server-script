4cc389ea52dd3a0160e3d181afe8301b5fd52311
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-17 16:40:38 +0900

    .

--- oraclelinux/util/storage_mount.sh
+++ oraclelinux/util/storage_mount.sh
@@ -4,17 +4,20 @@
 # echo -n "입력: "
 # read STORAGE_NAME
 
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
   STORAGE_NAME=/dev/sdb1
+  BACKUP_DIR=/storage_backup01/
+  echo "default 값으로 설정되었습니다."
+  echo "만약에 STORAGE_NAME과 BACKUP_DIR를 설정하고자 한다면, 파라미터를 넘겨주세요"
+  echo "($1:STORAGE_NAME, $2:BACKUP_DIR)"
 else
   STORAGE_NAME=$1
+  BACKUP_DIR=$2
+  echo "파라미터 2개에 의하여 설정되었습니다."
 fi
 
-if [ -z "$2" ]; then
-  BACKUP_DIR=/storage_backup01/
-else
-  BACKUP_DIR=$2
-fi
+echo "STORAGE_NAME : $STORAGE_NAME"
+echo "BACKUP_DIR : $BACKUP_DIR"
 
 
 
