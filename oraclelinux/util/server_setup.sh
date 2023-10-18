2987c0b82aa5bf2b1239472f5860540c1451b822
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-18 09:53:08 +0900

    .

--- oraclelinux/util/server_setup.sh
+++ oraclelinux/util/server_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TOTAL_STEP="6"
+CURRENT_STEP=1
 SLEEP_TIME=3
 
 echo "$SLEEP_TIME초 뒤, 유틸 서버 셋업 시작..."
@@ -20,18 +21,21 @@ GITLAB_SSH_PORT=23
 
 
 
-echo -e "(1/$TOTAL_STEP) dnf 업데이트 시작"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) dnf 업데이트 시작"
 if dnf list installed &> /dev/null; then
     echo "시스템 업데이트를 이미 수행했습니다. 업데이트를 건너뜁니다."
 else
 	dnf update -y
 fi
 
-echo -e "(1/$TOTAL_STEP) dnf 업데이트 완료!\n"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) dnf 업데이트 완료!\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
 
 
-echo -e "(2/$TOTAL_STEP) 도커 설치 시작"
+
+
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 도커 설치 시작"
 # Check if Docker is already installed
 if ! command -v docker &> /dev/null; then
     dnf install -y dnf-utils zip unzip
@@ -49,7 +53,8 @@ else
     echo "도커가 이미 설치되어있습니다."
 fi
 
-echo -e "(2/$TOTAL_STEP) 도커 설치 완료!\n"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 도커 설치 완료!\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
 
 
@@ -57,7 +62,7 @@ echo -e "(2/$TOTAL_STEP) 도커 설치 완료!\n"
 
 
 
-echo -e "(3/$TOTAL_STEP) GitLab 설치 시작"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) GitLab 설치 시작"
 
 # 폴더가 존재하지 않는 경우 생성
 if [ ! -d "$GITLAB_VOLUME_DIR" ]; then
@@ -106,21 +111,21 @@ else
     echo "GitLab이 설정되었습니다."
 fi
 
-echo -e "(3/$TOTAL_STEP) GitLab 설치 완료\n"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) GitLab 설치 완료\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
 
-
-echo -e "(4/$TOTAL_STEP) 방화벽 stop 및 자동 실행 제거 시작"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 방화벽 stop 및 자동 실행 제거 시작"
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo systemctl mask firewalld
-echo -e "(4/$TOTAL_STEP) 방화벽 stop 및 자동 실행 제거 완료\n"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 방화벽 stop 및 자동 실행 제거 완료\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
 
 
 
-
-echo -e "(5/$TOTAL_STEP) 서버 재부팅 시 gitlab 자동 실행 설정 시작"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 서버 재부팅 시 gitlab 자동 실행 설정 시작"
 file_name="/etc/systemd/system/docker-gitlab.service"
 new_content=$(cat <<EOF
 [Unit]
@@ -140,13 +145,13 @@ EOF
 
 echo "$new_content" > "$file_name"
 sudo systemctl enable docker-gitlab
-echo -e "(5/$TOTAL_STEP) 서버 재부팅 시 gitlab 자동 실행 설정 완료\n"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 서버 재부팅 시 gitlab 자동 실행 설정 완료\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
-
-echo -e "(6/$TOTAL_STEP) 백업 설정 디렉토리 생성 시작"
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 백업 설정 디렉토리 생성 시작"
 mkdir -p /storage/backup/log
-echo -e "(6/$TOTAL_STEP) 백업 설정 디렉토리 생성 완료\n"
-
+echo -e "($CURRENT_STEP/$TOTAL_STEP) 백업 설정 디렉토리 생성 완료\n"
+$CURRENT_STEP=$CURRENT_STEP+1
 
 
 echo -e "설치가 완료되었습니다"
