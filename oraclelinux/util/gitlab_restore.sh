177a2db177ff7a44327c394d623130c1ef815db4
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-18 10:09:31 +0900

    .

--- oraclelinux/util/gitlab_restore.sh
+++ oraclelinux/util/gitlab_restore.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 BACKUP_DESTINATION=/storage/gitlab/backups
-echo "백업 파일 이름을 입력해주세요"
+echo -n "백업 파일 이름 : "
 read FILE_NAME
 
-# gitlab 컨테이너 내부에 있는 psql 셋팅 해줘야함. restore 시, 아래 에러를 없애기 위함임
+# Gitlab 컨테이너 내부에 있는 psql 셋팅 해줘야함. restore 시, 아래 에러를 없애기 위함임
 # ERROR:  must be owner of extension pg_trgm
 # ERROR:  must be owner of extension btree_gist
 # ERROR:  must be owner of extension btree_gist
 # ERROR:  must be owner of extension pg_trgm
 
-
-# 복원할 백업 파일 경로 지정
-# GitLab 컨테이너 내에서 복원 명령 실행
+# Gitlab 관련 서비스 중지
 docker exec -it gitlab sh -c "gitlab-ctl stop unicorn"
 docker exec -it gitlab sh -c "gitlab-ctl stop puma"
 docker exec -it gitlab sh -c "gitlab-ctl stop sidekiq"
 
+
+# GitLab restore start
 chmod 777 $BACKUP_DESTINATION/*
-docker exec -it gitlab gitlab-backup restore BACKUP=$FILE_NAME  # YYYYMMDD를 복원할 백업 파일의 날짜로 대체
+docker exec -it gitlab gitlab-backup restore BACKUP=$FILE_NAME
 docker cp $BACKUP_DESTINATION/gitlab-secrets.json gitlab:/etc/gitlab/gitlab-secrets.json
 docker cp $BACKUP_DESTINATION/gitlab.rb gitlab:/etc/gitlab/gitlab.rb
 
