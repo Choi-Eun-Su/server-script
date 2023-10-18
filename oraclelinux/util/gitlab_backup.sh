fa6f110aa42018d020b43896821d63990fd1910f
Choi-Eun-Su <gfdhq11@gmail.com>
2023-10-18 10:28:19 +0900

    .

--- oraclelinux/util/gitlab_backup.sh
+++ oraclelinux/util/gitlab_backup.sh
@@ -12,7 +12,24 @@ docker cp gitlab:/etc/gitlab/gitlab.rb $BACKUP_DESTINATION/gitlab.rb
 
 # 백업 파일을 일정 기간 이상 보관하려면 추가 작업 필요
 # 예를 들어, 일주일 이상된 백업 파일을 삭제하는 로직을 추가할 수 있음
-~
-~
+# .tar 파일 목록 가져오기
+tar_files=("$BACKUP_DESTINATION"/*.tar)
+
+# .tar 파일 수 확인
+file_count=${#tar_files[@]}
+
+if [ $file_count -le 3 ]; then
+    echo "해당 폴더에 .tar 파일이 3개 이하로 존재합니다. 아무 파일도 삭제하지 않습니다."
+else
+    # .tar 파일을 파일 수정 시간을 기준으로 정렬
+    sorted_tar_files=($(ls -t "$BACKUP_DESTINATION"/*.tar))
+
+    # 가장 오래된 파일 선택
+    oldest_tar_file="${sorted_tar_files[-1]}"
+
+    # 선택된 파일 삭제
+    rm "$oldest_tar_file"
+    echo "가장 오래된 .tar 파일 \"$oldest_tar_file\"가 삭제되었습니다."
+fi
 ~
 ~
